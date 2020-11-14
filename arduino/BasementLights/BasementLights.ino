a04bfafd7549353459e92cbb3751460c09a756d5
Mike Urbanski <mikeurbanski1@gmail.com>
2020-11-14 13:06:00 -0600

    update

--- arduino/BasementLights/BasementLights.ino
+++ arduino/BasementLights/BasementLights.ino
@@ -19,23 +19,76 @@ int value = 0;
 
 void loop() {
 
-  if (initialized) {
-    delay(100);
-    return;
+  if (!initialized) {
+    int initLoopCount = 0;
+    while (Serial.available() == 0) {
+      digitalWrite(yellowPin, initLoopCount % 2);
+      initLoopCount = 1 - initLoopCount;
+      delay(200);
+    }
+  
+    initialized = true;
+    digitalWrite(yellowPin, LOW);
+  
+    delay(10);
   }
 
-  int initLoopCount = 0;
-  while (Serial.available() == 0) {
-    digitalWrite(yellowPin, initLoopCount % 2);
-    initLoopCount = 1 - initLoopCount;
-    delay(200);
+  if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('\n');
+    if (data.equals("BLUE_ON")) {
+      if (blueState == LOW) {
+        digitalWrite(bluePin, HIGH);
+        Serial.println("SUCCESS");
+        blueState = HIGH;
+      }
+      else {
+        Serial.println("NO_CHANGE");
+      }
+    }
+    else if (data.equals("GREEN_ON")) {
+      if (greenState == LOW) {
+        digitalWrite(greenPin, HIGH);
+        Serial.println("SUCCESS");
+        greenState = HIGH;
+      }
+      else {
+        Serial.println("NO_CHANGE");
+      }
+    }
+    if (data.equals("BLUE_OFF")) {
+      if (blueState == HIGH) {
+        digitalWrite(bluePin, LOW);
+        Serial.println("SUCCESS");
+        blueState = LOW;
+      }
+      else {
+        Serial.println("NO_CHANGE");
+      }
+    }
+    else if (data.equals("GREEN_OFF")) {
+      if (greenState == HIGH) {
+        digitalWrite(greenPin, LOW);
+        Serial.println("SUCCESS");
+        greenState = LOW;
+      }
+      else {
+        Serial.println("NO_CHANGE");
+      }
+    }
+    else {
+      Serial.println("Unknown command: '" + data + "'");
+    }
+    Serial.flush();
+    delay(10);
+  }
+  else {
+    digitalWrite(yellowPin, HIGH);
+    delay(50);
+    digitalWrite(yellowPin, LOW);
+    delay(50);
   }
 
-  initialized = true;
-  digitalWrite(yellowPin, LOW);
-  digitalWrite(bluePin, HIGH);
-
-  delay(1);
+  
 //
 //  if (initializing) {
 //    digitalWrite(yellowPin, HIGH);
--- python/main.py
+++ python/main.py
@@ -6,29 +6,26 @@ import time
 
 if __name__ == '__main__':
 
-    # if len(sys.argv) < 2:
-    #     print('Error: missing serial port arg', file=sys.stderr)
-    #     exit(1)
-    #
-    # port = sys.argv[1]
-    #
-    # ser = serial.Serial(port, 9600, timeout=1)
-    # ser.flush()
-    #
-    # print('Press enter')
-    # sys.stdin.readline()
-    # print('Sending initialization message')
-    # ser.write(b'Init\n')
-    # ser.flush()
+    if len(sys.argv) < 2:
+        print('Error: missing serial port arg', file=sys.stderr)
+        exit(1)
+
+    port = sys.argv[1]
+
+    ser = serial.Serial(port, 9600, timeout=1)
+    ser.flush()
+
+    print('Press enter')
+    sys.stdin.readline()
+    print('Sending initialization message')
+    ser.write(b'Init\n')
+    ser.flush()
 
     while True:
         cmd = input('Enter command: ')
-        print("'" + cmd + "'")
+        print(f'Sending command: "{cmd}"')
+        ser.write((cmd + '\n').encode('utf-8'))
+        ser.flush()
+        time.sleep(0.1)
+        print(f'Response: "{ser.readline().decode("utf-8").rstrip()}"')
 
-    # for line in sys.stdin:
-    #     ser.write((line.rstrip() + '\n').encode('utf-8'))
-    #     print(ser.readline().decode('utf-8').rstrip())
-    #     ser.flush()
-    #     print(ser.readline().decode('utf-8').rstrip())
-    #     ser.flush()
-    #     time.sleep(0.5)
