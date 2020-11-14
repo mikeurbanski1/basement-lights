313b0569ff705887b8713894dd76442d9b46a8ba
Mike Urbanski <mikeurbanski1@gmail.com>
2020-11-14 12:33:12 -0600

    add upload script

--- upload.sh
+++ upload.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-
+PORT=$(ls /dev/ttyACM*)
+FQBN=arduino:avr:uno
+arduino-cli compile -b arduino:avr:uno arduino/BasementLights
+arduino-cli upload -p $PORT -b arduino:avr:uno arduino/BasementLights
