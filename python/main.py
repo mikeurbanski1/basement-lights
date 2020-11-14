ff6e3972905fc7a4216022a9a5416879fdea9cd2
Mike Urbanski <mikeurbanski1@gmail.com>
2020-11-14 12:48:37 -0600

    update

--- python/main.py
+++ python/main.py
@@ -6,24 +6,25 @@ import time
 
 if __name__ == '__main__':
 
-    if len(sys.argv) < 2:
-        print('Error: missing serial port arg', file=sys.stderr)
-        exit(1)
-
-    port = sys.argv[1]
-
-    ser = serial.Serial(port, 9600, timeout=1)
-    ser.flush()
-
-    print('Press enter')
-    sys.stdin.readline()
-    print('Sending initialization message')
-    ser.write(b'Init\n')
-    ser.flush()
-
-    # ser.write(b'Hello\n')
-    # print(ser.readline().decode('utf-8').rstrip())
+    # if len(sys.argv) < 2:
+    #     print('Error: missing serial port arg', file=sys.stderr)
+    #     exit(1)
     #
+    # port = sys.argv[1]
+    #
+    # ser = serial.Serial(port, 9600, timeout=1)
+    # ser.flush()
+    #
+    # print('Press enter')
+    # sys.stdin.readline()
+    # print('Sending initialization message')
+    # ser.write(b'Init\n')
+    # ser.flush()
+
+    while True:
+        cmd = input('Enter command: ')
+        print("'" + cmd + "'")
+
     # for line in sys.stdin:
     #     ser.write((line.rstrip() + '\n').encode('utf-8'))
     #     print(ser.readline().decode('utf-8').rstrip())
