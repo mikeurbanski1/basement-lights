4466ccadf7a41342067ea5aaa54c3e008c011911
Mike Urbanski <mikeurbanski1@gmail.com>
2020-11-14 12:42:48 -0600

    update

--- python/main.py
+++ python/main.py
@@ -8,13 +8,19 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.flush()
 
-    ser.write(b'Hello\n')
-    print(ser.readline().decode('utf-8').rstrip())
+    print('Press enter')
+    sys.stdin.readline()
+    print('Sending initialization message')
+    ser.write(b'Init\n')
+    ser.flush()
 
-    for line in sys.stdin:
-        ser.write((line.rstrip() + '\n').encode('utf-8'))
-        print(ser.readline().decode('utf-8').rstrip())
-        ser.flush()
-        print(ser.readline().decode('utf-8').rstrip())
-        ser.flush()
-        time.sleep(0.5)
+    # ser.write(b'Hello\n')
+    # print(ser.readline().decode('utf-8').rstrip())
+    #
+    # for line in sys.stdin:
+    #     ser.write((line.rstrip() + '\n').encode('utf-8'))
+    #     print(ser.readline().decode('utf-8').rstrip())
+    #     ser.flush()
+    #     print(ser.readline().decode('utf-8').rstrip())
+    #     ser.flush()
+    #     time.sleep(0.5)
