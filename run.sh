db3ae06cde874212d8fe05ddf2ec1c672af0f427
Mike Urbanski <mikeurbanski1@gmail.com>
2020-11-21 16:22:45 -0600

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-gunicorn --bind unix:alexa.sock -m 777 python.wsgi:app -e COM_PORT="$(ls /dev/ttyACM* | x)"
+gunicorn --bind unix:alexa.sock -m 777 python.wsgi:app -e COM_PORT="$(ls /dev/ttyACM* | echo x)"
