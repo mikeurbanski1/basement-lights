5dd4d671d5653c173966a6f1f3e40413543dcc9c
Mike Urbanski <mikeurbanski1@gmail.com>
2020-11-21 16:09:11 -0600

    update

--- python/update_ngrok.py
+++ python/update_ngrok.py
@@ -4,51 +4,54 @@ import time
 import json
 import sys
 
-if os.path.exists('log.txt'):
-    os.remove('log.txt')
+while True:
+    if os.path.exists('log.txt'):
+        os.remove('log.txt')
 
-ngrok = subprocess.Popen(['ngrok', 'http', '--log', 'log.txt', '7626'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print('Starting ngrok')
+    ngrok = subprocess.Popen(['ngrok', 'http', '--log', 'log.txt', '7626'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-print(f'ngrok PID: {ngrok.pid}')
+    print(f'ngrok PID: {ngrok.pid}')
 
-time.sleep(2)
+    time.sleep(2)
 
-with open('log.txt', 'r') as fp:
-    for line in fp.readlines():
-        if 'https' in line:
-            index = line.index('https')
-            url = line[index:]
-            print(f'ngrok URL: {url}')
-            break
+    with open('log.txt', 'r') as fp:
+        for line in fp.readlines():
+            if 'https' in line:
+                index = line.index('https')
+                url = line[index:]
+                print(f'ngrok URL: {url}')
+                break
 
-get_manifest = subprocess.Popen(['ask', 'smapi', 'get-skill-manifest', '-s', 'amzn1.ask.skill.15dc0b67-8d15-4262-a35d-8333ae4568f0', '-g', 'development'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    get_manifest = subprocess.Popen(['ask', 'smapi', 'get-skill-manifest', '-s', 'amzn1.ask.skill.15dc0b67-8d15-4262-a35d-8333ae4568f0', '-g', 'development'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-stdout, stderr = get_manifest.communicate()
+    stdout, stderr = get_manifest.communicate()
 
-if stderr:
-    print('Got stderr output while getting manifest', file=sys.stderr)
-    print(stderr, file=sys.stderr)
+    if stderr:
+        print('Got stderr output while getting manifest', file=sys.stderr)
+        print(stderr, file=sys.stderr)
+        ngrok.kill()
+        exit(1)
+
+    manifest_config = json.loads(stdout)
+
+    print(manifest_config)
+
+    manifest_config['manifest']['apis']['custom']['endpoint']['uri'] = url
+
+    print(manifest_config)
+
+    update_manifest = subprocess.Popen(['ask', 'smapi', 'update-skill-manifest', '-s', 'amzn1.ask.skill.15dc0b67-8d15-4262-a35d-8333ae4568f0', '-g', 'development', '--manifest', json.dumps(manifest_config)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = update_manifest.communicate()
+
+    if stderr:
+        print('Got stderr output while updating manifest', file=sys.stderr)
+        print(stderr, file=sys.stderr)
+
+    print(stdout)
+
+    print('Sleeping for 7 hours (25200 seconds)')
+    time.sleep(25200)
+    print('Killing ngrok')
     ngrok.kill()
-    exit(1)
-
-manifest_config = json.loads(stdout)
-
-print(manifest_config)
-
-manifest_config['manifest']['apis']['custom']['endpoint']['uri'] = url
-
-print(manifest_config)
-
-update_manifest = subprocess.Popen(['ask', 'smapi', 'update-skill-manifest', '-s', 'amzn1.ask.skill.15dc0b67-8d15-4262-a35d-8333ae4568f0', '-g', 'development', '--manifest', json.dumps(manifest_config)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-stdout, stderr = update_manifest.communicate()
-
-if stderr:
-    print('Got stderr output while updating manifest', file=sys.stderr)
-    print(stderr, file=sys.stderr)
-
-print(stdout)
-
-print('Sleeping for 60 seconds')
-time.sleep(60)
-print('Killing ngrok')
-ngrok.kill()
+    time.sleep(1)
