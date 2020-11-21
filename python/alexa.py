7e941860df03395b4397e5aa03bad519bfa801d0
Mike Urbanski <mikeurbanski1@gmail.com>
2020-11-21 17:18:32 -0600

    update

--- python/alexa.py
+++ python/alexa.py
@@ -28,10 +28,10 @@ def setup_serial():
     global ser
     ports = [a for a in os.listdir('/dev') if 'ttyACM' in a]
     if ports:
-        try_port = ports[0]
+        try_port = os.path.join('/dev/', ports[0])
         if try_port != port:
             logger.info(f'Setting port to {try_port}')
-            port = os.path.join('/dev/', try_port)
+            port = try_port
 
             ser = serial.Serial(port, 9600, timeout=1)
             ser.flush()
