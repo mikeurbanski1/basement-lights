95f7c4734c5c7be0968e5610c14446ddf61477db
Mike Urbanski <mikeurbanski1@gmail.com>
2020-11-27 12:17:16 -0600

    update

--- python/update_ngrok.py
+++ python/update_ngrok.py
@@ -90,7 +90,7 @@ while True:
     logger.info(stdout)
 
     logger.info('Sleeping for 7 hours (25200 seconds)')
-    time.sleep(60)
+    time.sleep(25200)
     logger.info('Killing ngrok')
     ngrok.kill()
     time.sleep(5)
