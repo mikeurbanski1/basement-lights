5a2edf0989f545824f00bda0fcad634bca957809
Mike Urbanski <mikeurbanski1@gmail.com>
2020-11-27 12:01:49 -0600

    update

--- python/update_ngrok.py
+++ python/update_ngrok.py
@@ -1,3 +1,4 @@
+import logging
 import subprocess
 import os
 import time
@@ -7,17 +8,21 @@ import re
 
 pattern = re.compile(r'https://[a-z0-9]+\.ngrok\.io')
 
+logger = logging.getLogger(__name__)
+logger.setLevel('DEBUG')
+logger.addHandler(logging.StreamHandler())
+
 while True:
     if os.path.exists('log.txt'):
-        print('Removing old log.txt')
+        logger.debug('Removing old log.txt')
         os.remove('log.txt')
     else:
-        print('log.txt did not exist')
+        logger.debug('log.txt did not exist')
 
-    print('Starting ngrok')
+    logger.info('Starting ngrok')
     ngrok = subprocess.Popen(['ngrok', 'http', '--log', 'log.txt', '7626'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    print(f'ngrok PID: {ngrok.pid}')
+    logger.info(f'ngrok PID: {ngrok.pid}')
 
     time.sleep(5)  # slight delay for ngrok to run
 
@@ -35,7 +40,7 @@ while True:
         iterations += 1
 
     url = url.rstrip()
-    print(f'Found url" {url} (iterations: {iterations}')
+    logger.info(f'Found url: {url} (iterations: {iterations})')
 
     with open('url.txt', 'w') as fp:
         fp.write(url)
@@ -45,39 +50,42 @@ while True:
     stdout, stderr = get_manifest.communicate()
 
     if stderr:
-        print('Got stderr output while getting manifest', file=sys.stderr)
-        print(stderr, file=sys.stderr)
+        logger.critical('Got stderr output while getting manifest', file=sys.stderr)
+        logger.critical(stderr, file=sys.stderr)
         ngrok.kill()
         exit(1)
 
     manifest_config = json.loads(stdout)
 
-    print(manifest_config)
+    logger.info('Current manifest config:')
+    logger.info(json.dumps(manifest_config, indent=2))
 
     manifest_config['manifest']['apis']['custom']['endpoint']['uri'] = url
 
-    print(manifest_config)
+    logger.info('New manifest config:')
+    logger.info(json.dumps(manifest_config, indent=2))
 
     update_manifest = subprocess.Popen(['ask', 'smapi', 'update-skill-manifest', '-s', 'amzn1.ask.skill.15dc0b67-8d15-4262-a35d-8333ae4568f0', '-g', 'development', '--manifest', json.dumps(manifest_config)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = update_manifest.communicate()
 
     if stderr:
-        print('Got stderr output while updating manifest', file=sys.stderr)
-        print(stderr, file=sys.stderr)
+        logger.critical('Got stderr output while updating manifest', file=sys.stderr)
+        logger.critical(stderr, file=sys.stderr)
+        ngrok.kill()
+        exit(1)
 
-    print(stdout)
+    logger.info('Update skill response:')
+    logger.info(stdout)
 
     # send init notification
     curl = subprocess.Popen(['curl', f'{url}/hello'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = curl.communicate()
-    if stderr:
-        print('Got stderr output while sending curl command', file=sys.stderr)
-        print(stderr, file=sys.stderr)
 
-    print(stdout)
+    logger.error(f'Curl stderr: {stderr}')
+    logger.info(stdout)
 
-    print('Sleeping for 7 hours (25200 seconds)')
+    logger.info('Sleeping for 7 hours (25200 seconds)')
     time.sleep(25200)
-    print('Killing ngrok')
+    logger.info('Killing ngrok')
     ngrok.kill()
     time.sleep(5)
