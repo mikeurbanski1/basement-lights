73fe7ce19b47a22f8ab8e3127b398ad80da43094
Mike Urbanski <mikeurbanski1@gmail.com>
2020-12-09 19:06:17 -0600

    update isu

--- arduino/BasementLights/BasementLights.ino
+++ arduino/BasementLights/BasementLights.ino
@@ -155,7 +155,7 @@ String modeCommands[] = {
 int NUM_MODES = 13;
 
 // mode state
-int mode = 12;
+int mode = 11;
 boolean autoCycle = false;
 int modeIterationNumber = 0; // the current count of the outer mode repeat loop
 int modeLoopNumber = 0; // the current count of the inner loop for one cycle of a mode
